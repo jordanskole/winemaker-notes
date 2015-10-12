1f75cc05e5bb976eccf20a902f52cf25ba56a267
Jordan Skole <jordan@usebenchmark.com>
2015-10-12 13:17:37 -0400

    create note working

--- www/js/controllers/WineNoteCtrl.js
+++ www/js/controllers/WineNoteCtrl.js
@@ -1,5 +1,30 @@
-app.controller('WineNoteCtrl', ['$scope', '$state', '$stateParams', 'Auth', 'Wine', function($scope, $state, $stateParams, Auth, Wine) {
+app.controller('WineNoteCtrl', ['$scope', '$state', '$stateParams', 'Auth', 'Wine', 'Notes', function($scope, $state, $stateParams, Auth, Wine, Notes) {
 
   Wine.$bindTo($scope, 'wine');
+  $scope.note = {
+    "event": false,
+    "measurements": false,
+    "specific_gravity": null,
+    "color": null
+  };
+
+  $scope.createNote = function() {
+    console.log('Note:');
+    console.log($scope.note);
+
+    Notes.$array.$add({
+        "uid": Auth.$getAuth().uid,
+        "wine_id": Wine.$id,
+        "created_on": moment().format(),
+        "edited_on": moment().format(),
+        "message": $scope.note.message,
+        "event": $scope.note.event,
+        "measurements": $scope.note.measurements,
+        "specific_gravity": $scope.note.specific_gravity,
+        "color": $scope.note.color
+    }).then( function (ref) {
+      $state.go('wines.detail', {'id': Wine.$id });
+    });
+  };
 
 }]);
